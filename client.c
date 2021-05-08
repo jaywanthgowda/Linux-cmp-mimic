b29184cb07cdb0eb802f21e9ebe5e2253d77b188
Jaywanth J <54073644+jaywanthgowda11@users.noreply.github.com>
2021-05-08 11:32:02 +0530

    Update client.c

--- client.c
+++ client.c
@@ -7,10 +7,17 @@ int main(int argc, char **argv)
 {	
 	if(argc < 1)
 	{
+		//Condition where no parameters have been passed
 		exit(EXIT_FAILURE);
+	}
+	else if(argc == 1)
+	{
+		//Greeting condition and to redirect user to working of Program
+		initial_display_module();
 	}
 	else
 	{
+		//This redirects to the command functioning module where the backend comes into play and the errors are addressed.
 		initialise_program(argc,argv);
 	}
 	return 0;
