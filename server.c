82add994a5e0386e95a685246a5aeb75264787eb
Jaywanth J <54073644+jaywanthgowda11@users.noreply.github.com>
2021-05-08 11:33:06 +0530

    Update server.c

--- server.c
+++ server.c
@@ -2,15 +2,16 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<string.h>
-#include<ctype.h>
 #include "interface.h"
 
-//void initialise_program(int argc,char **argv);
-//Validation for file pointers
+//Function Declaration module
+//Validation for file pointer
 void file_handling_check(int ,char **);
 
 //cmp modules
 void cmp_usage(FILE *,FILE *,int,int,char **,int,int,int);
+
+//cmp sub-modules
 void cmp_base(FILE *,FILE *,int,int,char **);
 void cmp_b(FILE *,FILE *,int,int,char **);
 void cmp_n(FILE *,FILE *,int,int,int,char **);
@@ -20,53 +21,67 @@ int size_of_file(FILE *);
 int num_grab(char *);
 bool is_num_check(char);
 
+//This is used to greet the user regarding the functioning of the program
+void initial_display_module()
+{
+	printf("\n Welcome to my Program\n");
+	printf(" This program replicates the functioning of cmp command.\n");
+	printf(" The functioning of the program requires command line parameters just as you would to invoke cmp command on linux.\n");
+	printf(" An executable called output shall be created after using make.\n");
+	printf(" This executable should be used with cmp in a particular format as shown.\n");
+	printf(" Please use the following format\n");
+	printf("\toutput cmp file_1.ext file_2.ext [FLAGS] [SKIP1] [SKIP2]\n");
+	printf(" Flags supported are:\n");
+	printf(" (1.) -b flag : To display differing byte at the differentiating byte position.\n");
+	printf(" (2.) -n flag : To limit the number of bytes of the cmp command.To be used with an integer denoting LIMIT_bytes(also as a command-line parameter\n\n");
+}
 
+//This is the start of the program recognizing the command line arguments
 void initialise_program(int argc,char **argv)
 {	
-	if(argc > 1)
+	//Initial verification if command corresponding to cmp has been entered
+	if(strcmp(argv[1],"cmp")==0)
 	{
-		if(strcmp(argv[1],"cmp")==0)
+		if(argc>9)
 		{
-			if(argc>9)
-			{
-				printf("Invalid number of arguments.\n");
-			}
-			else if(argc == 2)
-			{
-				printf("Missing operands after 'cmp'.\n");
-			}
-			else if(argc == 3)
-			{
-				printf("cmp requires exactly 2 files to compare.\n");
-			}
-			else
-			{
-				file_handling_check(argc,argv);
-			}
+			//Arguments are more than the max criteria for cmp
+			printf("Invalid number of arguments.\n");
+		}
+		else if(argc == 2)
+		{
+			//If only cmp was entered
+			printf("Missing operands after 'cmp'.\n");
+		}
+		else if(argc == 3)
+		{
+			//If only one file following cmp has been entered
+			printf("cmp requires exactly 2 files to compare.\n");
 		}
 		else
 		{
-			printf("Invalid command.\nPlease enter cmp only.\n");
+			file_handling_check(argc,argv);
 		}
 	}
 	else
 	{
-		printf("No commands or files as source.\n");
-		printf("File assumes command line arguments of the form\nexecutable_file cmp file_to_be_compared_1 file_to_be_compared_2 [FLAGS] [SKIP1] [SKIP2]\n");
-		printf("FLAG and SKIP are optional");
-		printf("Please note all parameters passed are case-sensitive.\n");
-		exit(0);
+		printf("Invalid command.\nPlease enter cmp only.\n");
 	}
 }
 
+//This Deals with the initialisation of the file pointers and redirects based on specific command line arguments
 void file_handling_check(int argc,char **argv)
 {
+	//FILE Initialisation and access
 	FILE *f1,*f2;
-	f1=fopen(argv[2],"r");
-	f2=fopen(argv[3],"r");
+	f1=fopen(argv[2],"r+");
+	f2=fopen(argv[3],"r+");
+	
+	//Initial values -1 correspond to default conditions of cmp
 	int skip1=-1;
 	int skip2=-1;
 	int n_bytes=-1;
+	
+	//If fopen() fails or file does not exist
 	if(f1==NULL)
 	{
 		printf("%s does not exist in the directory or is invalid\n",argv[2]);
@@ -77,17 +92,24 @@ void file_handling_check(int argc,char **argv)
 		printf("%s does not exist in the directory or is invalid\n",argv[3]);
 		exit(1);
 	}
+	//If files have been successfully opened
 	else
 	{
-		//int n_parameter=0; 
+		//Corresponds to 4 arguments and cmp file1.ext file2.ext
 		if(argc == 4)
 		{
 			//Base cmp condition with no skip or flags
 			cmp_usage(f1,f2,0,argc,argv,0,0,0);
 		}
+		
 		else if(argc == 5)
 		{
-			//Condition is valid if flag = -b or bytes to be skipped from 1st file
+			/*
+				Corresponds to the following valid conditions
+				cmp file1.ext file2.ext -b
+				cmp file1.ext file2.ext skip1
+			*/
+				
 			if(strcmp(argv[4],"-b")==0)
 			{
 				cmp_usage(f1,f2,1,argc,argv,0,0,0);
@@ -106,6 +128,13 @@ void file_handling_check(int argc,char **argv)
 		}
 		else if(argc == 6)
 		{
+			/*
+				Corresponds to the following valid conditions
+				cmp file1.ext file2.ext -b skip1
+				cmp file1.ext file2.ext -n n_bytes
+				cmp file1.ext file2.ext skip1 skip2
+			*/
+			
 			if((strcmp(argv[4],"-b")==0)&&((skip1=num_grab(argv[5]))>-1))
 			{
 				cmp_usage(f1,f2,1,argc,argv,skip1,0,0);
@@ -125,7 +154,14 @@ void file_handling_check(int argc,char **argv)
 		}
 		else if(argc == 7)
 		{
-			//validation_for_7_arguments();
+			/*
+				Corresponds to the following valid conditions
+				cmp file1.ext file2.ext -b skip1 skip2
+				cmp file1.ext file2.ext -n n_bytes skip1
+				cmp file1.ext file2.ext -b -n n_bytes
+ 				cmp file1.ext file2.ext -n n_bytes -b
+			*/
+			
 			if((strcmp(argv[4],"-b")==0)&&((skip1=num_grab(argv[5]))>-1)&&((skip2=num_grab(argv[6]))>-1))
 			{
 				cmp_usage(f1,f2,1,argc,argv,skip1,skip2,0);
@@ -149,7 +185,13 @@ void file_handling_check(int argc,char **argv)
 		}
 		else if(argc == 8) 
 		{
-			//validation_for_8_arguments();
+			/*
+				Corresponds to the following valid conditions			
+				cmp file1.ext file2.ext -n n_bytes skip1 skip2
+				cmp file1.ext file2.ext -b -n n_bytes skip1
+				cmp file1.ext file2.ext -n n_bytes -b skip1
+			*/
+			
 			if((strcmp(argv[4],"-n")==0)&&((n_bytes=num_grab(argv[5]))>-1)&&((skip1=num_grab(argv[6]))>-1)&&((skip2=num_grab(argv[7]))>-1))
 			{
 				cmp_usage(f1,f2,2,argc,argv,skip1,skip2,n_bytes);
@@ -169,6 +211,11 @@ void file_handling_check(int argc,char **argv)
 		}
 		else if(argc == 9)
 		{
+			/*
+				Corresponds to the following valid conditions
+				cmp file1.ext file2.ext -b -n n_bytes skip1 skip2
+				cmp file1.ext file2.ext -n n_bytes -b skip1 skip2
+			*/
 			if((strcmp(argv[4],"-b")==0)&&(strcmp(argv[5],"-n")==0)&&((n_bytes=num_grab(argv[6]))>-1)&&((skip1=num_grab(argv[7]))>-1)&&((skip2=num_grab(argv[8]))>-1))
 			{
 				cmp_usage(f1,f2,1,argc,argv,skip1,skip2,n_bytes);
@@ -193,10 +240,10 @@ void file_handling_check(int argc,char **argv)
 
 
 	
-
-//0 base cmp with skip1 and skip 2
-//1 -b cmp with skip1 and skip 2
-//2 -n cmp with skip1 and skip 2
+/*This function redirects cmp based on n parameter leading to corresponding conditions as shown below
+	0 base cmp with skip1 and skip 2
+	1 -b cmp with skip1 and skip 2
+	2 -n cmp with skip1 and skip 2*/
 void cmp_usage(FILE *f1,FILE *f2,int n,int argc ,char **argv,int skip1,int skip2,int n_bytes)
 {
 	//OFFSET Module to find size of the files
@@ -227,11 +274,11 @@ void cmp_usage(FILE *f1,FILE *f2,int n,int argc ,char **argv,int skip1,int skip2
 	}
 }
 
-
+//Function to mimic cmp without flags
 void cmp_base(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
 {
-	char ch_from_file1,ch_from_file2;
-	int byte_count=0;
+	char ch_from_file1,ch_from_file2;//To store characters simulataneuosly from both files
+	int byte_count=0;//To store bytes
 	int line_count=1;
 	int flag=1;int diff_count=0;
 	
@@ -251,56 +298,8 @@ void cmp_base(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
 		ch_from_file1=fgetc(f1);
 		ch_from_file2=fgetc(f2);
 		byte_count++;
-		if(feof(f1))
-		{
-			printf("EOF on %s after byte %d,line %d\n",argv[2],byte_count,line_count-1);
-			flag=0;
-		}
-		if(feof(f2)&&flag)
-		{
-			printf("EOF on %s after byte %d,line %d\n",argv[3],byte_count,line_count-1);
-			flag=0;
-		}
-		if((ch_from_file1!=ch_from_file2)&&flag)
-		{
-			printf("%s %s differ: byte %d, line %d\n",argv[2],argv[3],byte_count,line_count);
-			diff_count++;
-			flag=0;
-		}
-		if((ch_from_file1=='\n')&&flag)
-		{
-			line_count++;
-		}
-	}while(flag);
-	if(diff_count==0)
-	{
-		printf("Files are identical byte-wise.\n");
-	}		
-}
-
-void cmp_b(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
-{
-	char ch_from_file1,ch_from_file2;
-	int byte_count=0;
-	int line_count=1;
-	int flag=1;int diff_count=0;
-	
-	//Module to skip bytes
-	if(skip1_bytes!=0)
-	{
-		fseek(f1,skip1_bytes,SEEK_SET);
-	}
-	
-	if(skip2_bytes!=0)
-	{
-		fseek(f2,skip2_bytes,SEEK_SET);
-	}
-	
-	do
-	{
-		ch_from_file1=fgetc(f1);
-		ch_from_file2=fgetc(f2);
-		byte_count++;
+		
+		//To check if EOF has occured
 		if(feof(f1))
 		{
 			printf("EOF on %s after byte %d,line %d\n",argv[2],byte_count,line_count);
@@ -311,12 +310,75 @@ void cmp_b(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
 			printf("EOF on %s after byte %d,line %d\n",argv[3],byte_count,line_count);
 			flag=0;
 		}
+		
+		//To check if bytes are different
 		if((ch_from_file1!=ch_from_file2)&&flag)
 		{
+			printf("%s %s differ: byte %d, line %d\n",argv[2],argv[3],byte_count,line_count);
+			diff_count++;
+			flag=0;
+		}
+		
+		//To check if a new line character has been found.
+		if((ch_from_file1=='\n')&&flag)
+		{
+			line_count++;
+		}
+	}while(flag);
+	
+	//Default display to show in case files are identical.
+	if(diff_count==0)
+	{
+		printf("Files are identical byte-wise.\n");
+	}		
+}
+
+//Function to mimic cmp with -b flag
+void cmp_b(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
+{
+	char ch_from_file1,ch_from_file2;//To store characters simulataneuosly from both files
+	int byte_count=0;//To store bytes
+	int line_count=1;
+	int flag=1;int diff_count=0;
+	
+	//Module to skip bytes
+	if(skip1_bytes!=0)
+	{
+		fseek(f1,skip1_bytes,SEEK_SET);
+	}
+	
+	if(skip2_bytes!=0)
+	{
+		fseek(f2,skip2_bytes,SEEK_SET);
+	}
+	
+	do
+	{
+		ch_from_file1=fgetc(f1);
+		ch_from_file2=fgetc(f2);
+		byte_count++;
+		
+		//To check if EOF has occured
+		if(feof(f1))
+		{
+			printf("EOF on %s after byte %d,line %d\n",argv[2],byte_count,line_count);
+			flag=0;
+		}
+		if(feof(f2)&&flag)
+		{
+			printf("EOF on %s after byte %d,line %d\n",argv[3],byte_count,line_count);
+			flag=0;
+		}
+		
+		//To check if bytes are different
+		if((ch_from_file1!=ch_from_file2)&&flag)
+		{
+			//Display differs as the differing characters are displayed.
 			printf("%s %s differ: byte %d, line %d.\n%x in %s and %x in %s",argv[2],argv[3],byte_count,line_count,ch_from_file1,argv[2],ch_from_file2,argv[3]);
 			diff_count++;
 			flag=0;
 		}
+		//To check if a new line character has been found.
 		if((ch_from_file1=='\n')&&flag)
 		{
 			line_count++;
@@ -328,9 +390,10 @@ void cmp_b(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,char **argv)
 	}		
 }
 
+//Function to mimic cmp with -n flag
 void cmp_n(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,int n_bytes,char **argv)
 {
-	char ch_from_file1,ch_from_file2;
+	char ch_from_file1,ch_from_file2;//To store characters simulataneuosly from both files
 	int byte_count=0;
 	int line_count=1;
 	int flag=1;int diff_count=0;
@@ -351,6 +414,7 @@ void cmp_n(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,int n_bytes,char **
 		ch_from_file1=fgetc(f1);
 		ch_from_file2=fgetc(f2);
 		byte_count++;
+		//To check if EOF has occured
 		if(feof(f1))
 		{
 			printf("EOF on %s after byte %d,line %d\n",argv[2],byte_count,line_count);
@@ -361,24 +425,28 @@ void cmp_n(FILE *f1,FILE *f2,int skip1_bytes,int skip2_bytes,int n_bytes,char **
 			printf("EOF on %s after byte %d,line %d\n",argv[3],byte_count,line_count);
 			flag=0;
 		}
+		
+		//To check if bytes are different
 		if((ch_from_file1!=ch_from_file2)&&flag)
 		{
 			printf("%s %s differ: byte %d, line %d\n",argv[2],argv[3],byte_count,line_count);
 			diff_count++;
 			flag=0;
 		}
+		//To check if a new line character has been found.
 		if((ch_from_file1=='\n')&&flag)
 		{
 			line_count++;
 		}
 	}while(flag && byte_count<n_bytes);
+	//While condition differs and stops when byte_count reaches n_bytes
 	if(diff_count==0)
 	{
 		printf("Files are identical byte-wise upto %d bytes.\n",n_bytes);
 	}		
 }
 
-
+//To find the number of bytes of a file
 int size_of_file(FILE *fp)
 {
 	if(fp==NULL)
@@ -391,6 +459,7 @@ int size_of_file(FILE *fp)
 	return size;
 }
 
+//To convert the command-line argument to the corresponding integer
 int num_grab(char *str)
 {
 	if (*str == '\0')
@@ -398,29 +467,26 @@ int num_grab(char *str)
 		return -1;
 	}
 	long res = 0;
-	long i = 0;
+	long counter = 0;
 
 	if (str[0] == '-')
 	{
 		return -1;
 	}
 	
-	for (; str[i] != '\0'; ++i)
+	for (; str[counter] != '\0'; ++counter)
 	{
-		if (is_num_check(str[i]) == false)
+		if (is_num_check(str[counter]) == false)
 		{
 			return -1;
 		}
-		res = res * 10 + str[i] - '0';
+		res = res * 10 + str[counter] - '0';
 	}
 	return res;
 }
 
+//To check if the character is specifically a number
 bool is_num_check(char ch)
 {
 	return (ch>='0' && ch<='9')?true:false;
 }
-
-
-
-
